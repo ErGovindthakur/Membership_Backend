0ae6baf17d151d991c6a7803d3ceda9eeb500ed1
ErGovindthakur <ergovindthakur@gmail.com>
2025-03-13 18:21:10 +0530

    Added config

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
 import app from "./src/app";
+import { myConfig } from "./src/config/config";
 
 const startServer = () => {
-  const port = process.env.PORT || 3000;
+  const port = myConfig.port || 3000;
 
   app.listen(port, () => {
     console.log(`Server is running at port http://localhost:${port}`);
---
+++ src/config/config.ts
@@ -0,0 +1,8 @@
+import { config } from "dotenv";
+config();
+
+const _config = {
+     port:process.env.PORT
+};
+
+export const myConfig = Object.freeze(_config);
