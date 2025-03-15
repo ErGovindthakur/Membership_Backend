ca9b23f6c7e6a61ee35b04dbbc0834b174227179
ErGovindthakur <ergovindthakur@gmail.com>
2025-03-15 11:20:12 +0530

    Create userRouter

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,13 @@
 import express from "express";
-import createHttpError from 'http-errors';
- // Ensure this is correct
-
 import { globalErrorHandler } from "./middleware/globalErrorHandler";
+import userRouter from "./user/userRouter";
 
 
 const app = express();
 
-app.get("/", (req, res) => {
-  const error = createHttpError(400,"Something went wrong");
-  throw error;
-});
+// routes as middleware
+
+app.use("/api/users",userRouter);
 
 
 // creating global error handler and it's a middleware (must be at last of all routes), it generally contains four parameter
