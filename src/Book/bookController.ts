44e99230b16b40e5feb332c1442655713b1e5728
ErGovindthakur <ergovindthakur@gmail.com>
2025-03-17 05:35:01 +0530

    setup multer

--- src/Book/bookController.ts
+++ src/Book/bookController.ts
@@ -2,6 +2,9 @@ import { Request, Response, NextFunction } from "express";
 
 
 const createBook = (req: Request, res: Response, next: NextFunction) => {
+     const {} = req.body;
+     // when data comes through forms
+     
      res.json({message:"ok"})
 };
 
